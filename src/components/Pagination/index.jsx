511a2cd7aff7e2256a41c5dfc0ef2ebd0f63df94
Thanh <phu0nm2@gmail.com>
2022-08-22 19:39:23 +0700

    add file pagination to deploy

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+
+// const Pagination = (props) => {
+//   const pageLinks = [];
+
+//   for (let i = 0; i <= props.pages + 1; i++) {
+//     const active = props;
+//   }
+//   return <div>Pagination</div>;
+// };
+
+// export default Pagination;
